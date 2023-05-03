620093b42ad14d2f50402289f17b4ac44732ab0d
maria <maria@WIN-L58HK5EKJ6M>
2023-05-03 23:15:03 +0300

    Добавьте файлы проекта.

---
+++ Піцерія/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace PizzaMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.comboBoxSize = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name1,
+            this.Size,
+            this.Price,
+            this.Description});
+            this.dataGridView1.Location = new System.Drawing.Point(27, 121);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(602, 213);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(82, 82);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // comboBoxSize
+            // 
+            this.comboBoxSize.FormattingEnabled = true;
+            this.comboBoxSize.Items.AddRange(new object[] {
+            "30cм",
+            "45см"});
+            this.comboBoxSize.Location = new System.Drawing.Point(211, 82);
+            this.comboBoxSize.Name = "comboBoxSize";
+            this.comboBoxSize.Size = new System.Drawing.Size(98, 24);
+            this.comboBoxSize.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(651, 121);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(104, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Додати";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "назва піци";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(329, 82);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPrice.TabIndex = 5;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(456, 82);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDescription.TabIndex = 6;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(651, 166);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(104, 23);
+            this.buttonEdit.TabIndex = 7;
+            this.buttonEdit.Text = "Редагувати";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(651, 214);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(104, 25);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Виділити";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(217, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "розмір піци";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ціна піци";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(468, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "опис піци";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(651, 264);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(104, 25);
+            this.buttonSave.TabIndex = 12;
+            this.buttonSave.Text = "Зберегти";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(651, 311);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(104, 23);
+            this.buttonLoad.TabIndex = 13;
+            this.buttonLoad.Text = "Завантажити";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "назва піци";
+            this.Name1.MinimumWidth = 6;
+            this.Name1.Name = "Name1";
+            this.Name1.ReadOnly = true;
+            this.Name1.Width = 125;
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "розмір піци";
+            this.Size.MinimumWidth = 6;
+            this.Size.Name = "Size";
+            this.Size.ReadOnly = true;
+            this.Size.Width = 125;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "ціна піци";
+            this.Price.MinimumWidth = 6;
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            this.Price.Width = 125;
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "опис піци";
+            this.Description.MinimumWidth = 6;
+            this.Description.Name = "Description";
+            this.Description.ReadOnly = true;
+            this.Description.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 346);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.comboBoxSize);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Піцерія";
+            this.Click += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ComboBox comboBoxSize;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+    }
+}
+
---
+++ Піцерія/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace PizzaMenu
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+           
+            if (textBoxName.Text == "" || textBoxPrice.Text == "" || textBoxDescription.Text == "" || comboBoxSize.SelectedItem == null)
+            {
+                MessageBox.Show("Будь ласка, заповніть всі поля!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int n = dataGridView1.Rows.Add();
+            dataGridView1.Rows[n].Cells[0].Value = textBoxName.Text;
+            dataGridView1.Rows[n].Cells[1].Value = comboBoxSize.SelectedItem.ToString();
+            dataGridView1.Rows[n].Cells[2].Value = textBoxPrice.Text;
+            dataGridView1.Rows[n].Cells[3].Value = textBoxDescription.Text;
+
+            textBoxName.Clear();
+            comboBoxSize.SelectedItem = null;
+            textBoxPrice.Clear();
+            textBoxDescription.Clear();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Будь ласка, оберіть рядок для редагування!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (comboBoxSize.SelectedItem == null)
+            {
+                MessageBox.Show("No size!");
+                return;
+            }
+
+            int rowIndex = dataGridView1.CurrentRow.Index;
+                dataGridView1.Rows[rowIndex].Cells[0].Value = textBoxName.Text;
+                dataGridView1.Rows[rowIndex].Cells[1].Value = comboBoxSize.SelectedItem.ToString();
+                dataGridView1.Rows[rowIndex].Cells[2].Value = textBoxPrice.Text;
+                dataGridView1.Rows[rowIndex].Cells[3].Value = textBoxDescription.Text;
+            
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+  
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Будь ласка, оберіть рядок для видалення!", "Примітка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+            }
+            dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            textBoxName.Clear();
+            comboBoxSize.SelectedItem = null;
+            textBoxPrice.Clear();
+            textBoxDescription.Clear();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+ 
+            string fileName = "menu.xml";
+            XmlWriter writer = XmlWriter.Create(fileName);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("menu");
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                writer.WriteStartElement("pizza");
+                writer.WriteElementString("name", row.Cells[0].Value.ToString());
+                writer.WriteElementString("size", row.Cells[1].Value.ToString());
+                writer.WriteElementString("price", row.Cells[2].Value.ToString());
+                writer.WriteElementString("description", row.Cells[3].Value.ToString());
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+
+            MessageBox.Show("Дані успішно збережені в файл " + fileName, "Збереження", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+           
+            dataGridView1.Rows.Clear();
+
+            string fileName = "menu.xml";
+            if (File.Exists(fileName))
+            { 
+                XmlReader reader = XmlReader.Create(fileName);
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "pizza")
+                    {
+                        string name = "";
+                        string size = "";
+                        string price = "";
+                        string description = "";
+
+                        while (reader.Read())
+                        {
+                            if (reader.NodeType == XmlNodeType.Element)
+                            {
+                                switch (reader.Name)
+                                {
+                                    case "name":
+                                        reader.Read();
+                                        name = reader.Value;
+                                        break;
+                                    case "size":
+                                        reader.Read();
+                                        size = reader.Value;
+                                        break;
+                                    case "price":
+                                        reader.Read();
+                                        price = reader.Value;
+                                        break;
+                                    case "description":
+                                        reader.Read();
+                                        description = reader.Value;
+                                        break;
+                                }
+                            }
+
+                            if (reader.NodeType == XmlNodeType.EndElement && reader.Name == "pizza")
+                            {
+                                int n = dataGridView1.Rows.Add();
+                                dataGridView1.Rows[n].Cells[0].Value = name;
+                                dataGridView1.Rows[n].Cells[1].Value = size;
+                                dataGridView1.Rows[n].Cells[2].Value = price;
+                                dataGridView1.Rows[n].Cells[3].Value = description;
+                                break;
+                            }
+                        }
+                    }
+                }
+                reader.Close();
+
+                MessageBox.Show("Дані успішно завантажені з файлу " + fileName, "Завантаження", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Файл " + fileName + " не знайдено!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            int rowIndex = dataGridView1.CurrentRow.Index;
+            textBoxName.Text = "" + dataGridView1.Rows[rowIndex].Cells[0].Value;
+            comboBoxSize.SelectedItem = dataGridView1.Rows[rowIndex].Cells[1].Value;
+            textBoxPrice.Text = "" + dataGridView1.Rows[rowIndex].Cells[2].Value;
+            textBoxDescription.Text = "" + dataGridView1.Rows[rowIndex].Cells[3].Value;
+        }
+    }
+}
